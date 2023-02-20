e48be5cf248fdb8919d31fbb53b7121337fd946c
Vlad B <belousov7vlad@gmail.com>
2023-02-20 14:39:19 +0300

    route fix

--- src/utils/hooks/ProtectedRoute.js
+++ src/utils/hooks/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ isLogged }) => {
-  return isLogged ? <Outlet /> : <Navigate to="/welcome" />;
+  return isLogged ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;
